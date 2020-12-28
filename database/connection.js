0c63153dcdc35ac696d5d5770153a90c301491a0
Elias Biondo <contato@eliasbiondo.com>
2020-12-28 19:35:12 -0300

    sqlite support

--- database/connection.js
+++ database/connection.js
@@ -3,9 +3,9 @@ const env = require('dotenv');
 
 env.config()
 
-const connection = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
+const connection = new Sequelize({
+    dialect: 'sqlite',
+    storage: './database/database.sqlite',
     logging: false
 })
 
