6a43a90004fd17fc5c1c4db1d8bb0496653e6c87
Elias Biondo <contato@eliasbiondo.com>
2020-12-28 19:11:51 -0300

    POST route added

--- routes.js
+++ routes.js
@@ -1,9 +1,45 @@
 const express = require('express');
-
 const routes = express.Router();
+const Questions = require('./models/Questions');
 
 routes.get('/', (req, res) => {
-    return res.send('Hello, world!');
+
+    Questions.findAll({raw: true})
+
+    Questions.findAll({
+        raw: true,
+        order: [
+            ['id', 'DESC']
+        ],
+        limit:5
+    }).then(questions => {
+        res.render('index', {
+            questions
+        });
+    })
+
+
+})
+
+routes.get('/perguntar', (req, res) => {
+    return res.render('perguntar');
+})
+
+routes.post('/perguntar/salvarpergunta', (req, res) => {
+    const title = req.body.title;
+    const description = req.body.description;
+
+    if(title == '' || description == '') {
+        res.redirect('/perguntar')
+    } else {
+        Questions.create({
+            title,
+            description
+        }).then(() => {
+            res.redirect('/')
+        })
+    }
+
 })
 
 module.exports = routes;
